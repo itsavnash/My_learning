780efba2acc63a566ffb9b2f83ffd673f46a3f11
Avinash <avinash.studyai@gmail.com>
2025-11-01 21:25:54 +0530

    Delete Applied  Data Science/1-Introduction to Data Science in Python/1.1-Basic data preprocessing with pandas/01_Intel_Chipset_10.1.24.5/01_Intel_Chipset_10.1.24.5 directory

--- Applied  Data Science/1-Introduction to Data Science in Python/1.1-Basic data preprocessing with pandas/01_Intel_Chipset_10.1.24.5/01_Intel_Chipset_10.1.24.5/Install.bat
+++
@@ -1,8 +0,0 @@
-@echo *** Chipset Driver installation ***
-
-pnputil -a "%~dp0drivers\DriverFiles\production\Windows10-x64\TigerlakePCH-LPSystem.inf" /install
-pnputil -a "%~dp0drivers\DriverFiles\production\Windows10-x64\TigerlakePCH-LPDmaSecExtension.inf" /install
-pnputil -a "%~dp0drivers\DriverFiles\production\Windows10-x64\TigerlakePCH-LPSystemLPSS.inf" /install
-
-exit
-
